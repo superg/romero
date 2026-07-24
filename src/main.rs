520d6c4b634c8ca988a524ade6d4f049813e1534
Hennadiy Brych <gennadiy.brich@gmail.com>
2026-07-24 08:16:53 -0400

    cleanup of old files

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use std::error::Error;
-
-fn main() -> Result<(), Box<dyn Error>> {
-    let args: Vec<String> = std::env::args().collect();
-
-    let config = romero::Config::build(&args)?;
-
-    romero::run(config)
-}
